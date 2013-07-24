38af14638f235ab641a73dd6e2a3b7dc87423389
Norman Gray <norman@astro.gla.ac.uk>
2013-07-24 17:33:39 +0000

    Various updates to text and to list of known units

--- known-units-to-tex.py
+++ known-units-to-tex.py
@@ -8,12 +8,48 @@ lines = []
 
 for line in sys.stdin:
     if not re.search('#',line):
-        lines.append(line.split(','))
+        lines.append(line.strip().split(','))
 
 nrows = (len(lines)+1)/2
 
+# The name-generating hack below doesn't work well in all cases,
+# so supply some explicit replacements.
+replacements = { 'qudt:AstronomicalUnit': "astro'l unit",
+                 'unity:DetectorChannel': 'channel',
+                 'qudt:DegreeAngle': 'degree (angle)',
+                 'unity:DistributionBin': 'bin',
+                 'qudt:Gram': 'gramme',
+                 'unity:MilliArcSecond': 'milliarcsecond',
+                 'qudt:MinuteTime': 'minute (time)',
+                 'qudt:SecondTime': 'second (time)',
+                 'unity:SolarLuminosity': 'luminosity',
+                 'unity:StellarMagnitude': 'magnitudes',
+                'qudt:UnifiedAtomicMassUnit': 'AMU',
+                 }
+
+all_uppercase = re.compile('^[A-Z]+$')
+initial_capital = re.compile('[A-Z][^A-Z]*')
+
+def fmtname(uname):
+    if uname in replacements:
+        rval = replacements[uname]
+    else:
+        nameparts = uname.split(':')
+        if len(nameparts) != 2:
+            print >>sys.stderr, "name %s malformed" % uname
+            rval ='XXX'
+        else:
+            tname = nameparts[1]
+            if all_uppercase.match(tname):
+                rval = tname
+            elif initial_capital.match(tname):
+                rval = string.join(map(string.lower, initial_capital.findall(tname)), ' ')
+            else:
+                rval = tname
+    return [rval]
+
 def fmtrow(r):
-    return string.join([r[0]] + (map(lambda s: "$\\cdot$" if s=="1" else s[1:], r[2:])),'&')
+    return string.join([r[0]] + fmtname(r[1]) + (map(lambda s: "$\\cdot$" if s=="1" else s[1:], r[2:])),'&')
 
 for r in range(nrows):
     r2 = r+nrows
